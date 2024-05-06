6608726ab4e3900640297b8fd8be53307915c092
dortiz257 <114173482+dortiz257@users.noreply.github.com>
2024-05-05 22:41:13 -0400

    Update compiler.rkt

--- compiler.rkt
+++ compiler.rkt
@@ -122,34 +122,31 @@
   (match e
     ;; literals
     [(? integer? i) i]
-    ['true 'todo]
-    ['false 'todo]
-    [(? symbol? x) 'todo]
-    [`(,(? bop? bop) ,e0 ,e1) 'todo]
-    [`(,(? uop? uop) ,e) 'todo]
-    ;; 0-binding case
-    [`(let* () ,e) 'todo]
-    ;; 1+-binding case
-    [`(let* ([,(? symbol? x0) ,e0]) ,e-body)
-     'todo]
-    [`(let* ([,(? symbol? x0) ,e0] ,rest-binding-pairs ...) ,e-body)
-     'todo]
+    ['true e]
+    ['false e]
+    [(? symbol? x) x]
+    [`(,(? bop? bop) ,e0 ,e1) `(,bop ,(ifarith->ifarith-tiny e0) ,(ifarith->ifarith-tiny e1))]
+    [`(,(? uop? uop) ,e) `(,uop ,(ifarith->ifarith-tiny e))]
+    [`(let* ([,(? symbol? x) ,e]) ,e-body)
+     `(let ([,x ,((ifarith->ifarith-tiny e))] ,(ifarith->ifarith-tiny e-body)))]
+    [`(let* ([,(? symbol? x) ,e] ,rest-binding-pairs ...) ,e-body)
+     `(let ([,x ,(ifarith->ifarith-tiny e)]) ,(ifarith->ifarith-tiny `(let* (,@rest-binding-pairs) ,e-body)))]
     ;; print an arbitrary expression (must be a number at runtime)
-    [`(print ,_)
-     'todo]
+    [`(print ,_) e]
     ;; and/or, with short-circuiting semantics
-    [`(and ,e0) 'todo]
-    [`(and ,e0 ,es ...) 'todo]
-    [`(or ,e0) 'todo]
-    [`(or ,e0 ,es ...) 'todo]
+    [`(and ,e0) (ifarith->ifarith-tiny e0)]
+    [`(and ,e0 ,es ...) (ifarith->ifarith-tiny `(if ,e0 (and ,@es) 0))]
+    [`(or ,e0) (ifarith->ifarith-tiny e0)]
+    [`(or ,e0 ,es ...) (ifarith->ifarith-tiny `(if ,e0 true (or ,es)))]
     ;; if argument is 0, false, otherwise true
-    [`(if ,e0 ,e1 ,e2) 'todo]
+    [`(if ,e0 ,e1 ,e2) `(if ,(ifarith->ifarith-tiny e0)
+                            ,(ifarith->ifarith-tiny e1)
+                            ,(ifarith->ifarith-tiny e2))]
     ;; cond where the last case is else
     [`(cond [else ,(? ifarith? else-body)])
-     'todo]
+     (ifarith->ifarith-tiny else-body)]
     [`(cond [,c0 ,e0] ,rest ...)
-     'todo]))
-
+     (ifarith->ifarith-tiny `(if ,c0 ,e0 (cond ,@rest)))]))
 ;; Stage 3: Administrative Normal Form (ANF)
 ;; 
 ;; In administrative normal form (or A-normal form) breaks up complex
@@ -509,7 +506,7 @@
    ; return the argument as a filename to compile
    filename))
 
-(define file-to-write (string-append (first (string-split filename ".")) ".asm")) 
+(define file-to-write (string-append (first (string-split filename ".")) ".asm"))
 (define object-file (string-append (first (string-split filename ".")) ".o")) 
 (define exe-file (string-append (first (string-split filename ".")))) 
 
